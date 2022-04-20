47bb5f8722fb74f929ae68dd10c6c1a96a79c20c
J Lee <jay.j.lee.1@gmail.com>
2022-04-20 12:24:04 -0400

    solutioned

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,7 +1,17 @@
 function balancingParentheses(string) {
   // type your code here
-}
+  let openCount = 0;
+  let closeCount = 0;
+  
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === '(') {++openCount}
+    else if (openCount >0) {--openCount}
+    else (++closeCount)
+  }
+  return openCount+closeCount
 
+}
+// )(
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 0");
@@ -21,4 +31,5 @@ if (require.main === module) {
 module.exports = balancingParentheses;
 
 // Please add your pseudocode to this file
+// if '(' need to have a ')' or add to count. count of '(' === ')'. if not add to count
 // And a written explanation of your solution
